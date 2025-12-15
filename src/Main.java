d1539d1164ae506ab11eb914aab77798a928e601
boitumelo-09 <belmiredub4@gmail.com>
2025-12-15 15:57:18 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
 
         System.out.println("  Welcome Bro... ");
-        System.out.println("...................");
+        System.out.println("..................");
 
         System.out.print("Enter your name bro : ");
         String name = scanner.nextLine();
